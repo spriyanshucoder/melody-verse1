eb0be4154e08d7339fa88d8d386e388770296595
Priyanshu Singh <s.priyanshu.coder@gmail.com>
2024-06-01 09:26:34 +0530

    create  and run the server

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app=express();
+app.listen(3000, ()=>{
+    console.log('Serverr is running on port 3000')
+});
+
+
+
+
+
+//backend/api me hai to kya karna hai run ke liye node ./api/index.js     cd.. for root after that  npm init-y for package.json make folder on melody verse after that install express after that write common express after that package.json change type: module afte that better package.json me script me we will add dev: nodemon api/index.js  start: node api/index.js then if i run npm run dev ----nodemon yeah
+//move gitignore to client side
