823e90633e7432afa56e753edf9b7a331a8641da
Priyanshu Singh <s.priyanshu.coder@gmail.com>
2024-06-06 20:22:16 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 // Connect to MongoDB
@@ -19,7 +20,7 @@ app.listen(3000, () => {
 });
 
 
-
+app.use("/api/user", userRoutes);
 
 
 //backend/api me hai to kya karna hai run ke liye node ./api/index.js     cd.. for root after that  npm init-y for package.json make folder on melody verse after that install express after that write common express after that package.json change type: module afte that better package.json me script me we will add dev: nodemon api/index.js  start: node api/index.js then if i run npm run dev ----nodemon yeah
