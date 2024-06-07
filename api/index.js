9ea8da5cebd9a53820e30c13133bbdaa4941ed43
Priyanshu Singh <s.priyanshu.coder@gmail.com>
2024-06-08 00:20:12 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 // Connect to MongoDB
@@ -15,12 +16,15 @@ mongoose.connect(process.env.MONGO_URI)
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000  ');
 });
 
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 
 //backend/api me hai to kya karna hai run ke liye node ./api/index.js     cd.. for root after that  npm init-y for package.json make folder on melody verse after that install express after that write common express after that package.json change type: module afte that better package.json me script me we will add dev: nodemon api/index.js  start: node api/index.js then if i run npm run dev ----nodemon yeah
